deba57625b54c15a36152d5fde81830c34fbb259
yubathom <yubathom@gmail.com>
2019-05-10 05:14:38 -0300

    Desinstala plugin-demo-block

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -1,17 +1,4 @@
 module.exports = {
-
-    head: [
-        ['script', { src: 'https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js' }],
-        ['script', { src: 'https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js' }],
-        ['script', { src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js' }],
-        ['script', { src: 'https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js' }],
-    ],
-    plugins: [
-        'demo-block'
-    ],
-
-
-
     locales: {
         "/": {
             lang: "pt-br",
